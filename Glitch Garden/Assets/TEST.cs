0451e0494cf0be3082c80778d88e633554d682df
Renzo Garcia <garcia.enzo54@gmail.com>
2017-10-06 18:37:50 +0800

    v0.06
    v0.02
    * Introduced Ui sliders. (Volume and Difficulty)
    * Default Button implimented.
    
    v0.03
    * Started adding characters with animation.
    
    v0.04
    * World UI Scaling
    
    v0.05
    * Started animating the Lizard.
    
    v0.06
    * Added walking and attacking animation to the Lizard
    * Fixed fuzzy lizard.

--- Glitch Garden/Assets/TEST.cs
+++ Glitch Garden/Assets/TEST.cs
@@ -6,15 +6,6 @@ public class TEST : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print (PlayerPrefsManager.GetMasterVolume ());
-		PlayerPrefsManager.SetMasterVolume (0.3f);
-		print (PlayerPrefsManager.GetMasterVolume ());
-
-		print (PlayerPrefsManager.IsLevelUnlocked (2));
-		PlayerPrefsManager.UnlockLevel (2);
-		print (PlayerPrefsManager.IsLevelUnlocked (2));
-
-		print (PlayerPrefsManager.GetDifficulty ());
-		PlayerPrefsManager.SetDifficulty (1f);
 		print (PlayerPrefsManager.GetDifficulty ());
 	}
 	
